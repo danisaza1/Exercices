a7aeb9d979342e11af3ba902dc664f8a6b600c75
danisaza1 <dani_isaza@htomail.com>
2025-04-10 10:17:48 +0200

    "add todo bien "

--- exercices_branches/code_clean/refractoring2.js
+++ exercices_branches/code_clean/refractoring2.js
@@ -1,21 +1,30 @@
+function informationsUtilisateur(donneesPersoBrut, donneesPersoutilisateur) {
+    return `${donneesPersoBrut} : ${donneesPersoutilisateur}\n`;
+  }
+  
+function statutAbonnement(estAbonne) {
+    return informationsUtilisateur("Statut", estAbonne ? "Abonné" : "Non abonné");
+    // el operador ? : es una forma corta de escribir una 
+    // condicion en una sola linea entonces es 
+    // condicion ? valor si true : valeur si false
+}
+
+
+function categorieAge(age) {
+if (age < 18) return informationsUtilisateur("Catégorie", "Jeune");
+if (age < 60) return informationsUtilisateur("Catégorie", "Adulte");
+return informationsUtilisateur("Catégorie", "Senior");
+}  
+
 function genererRapportUtilisateur(nom, age, ville, profession, estAbonne) {
-    let rapport = "Rapport utilisateur :\\n";
-    rapport += "Nom : " + nom + "\\n";
-    rapport += "Age : " + age + " ans\\n";
-    rapport += "Ville : " + ville + "\\n";
-    rapport += "Profession : " + profession + "\\n";
-    if (estAbonne == true) {
-      rapport += "Statut : Abonné\\n";
-    } else {
-      rapport += "Statut : Non abonné\\n";
-    }
-    if (age < 18) {
-      rapport += "Catégorie : Jeune\\n";
-    } else if (age >= 18 && age < 60) {
-      rapport += "Catégorie : Adulte\\n";
-    } else {
-      rapport += "Catégorie : Senior\\n";
-    }
+    let rapport = "Rapport utilisateur =\n";
+    rapport += informationsUtilisateur("Nom", nom);
+    rapport += informationsUtilisateur("Age", `${age} ans`);
+    rapport += informationsUtilisateur("Ville", ville);
+    rapport += informationsUtilisateur("Profession", profession);
+    rapport += statutAbonnement(estAbonne);
+    rapport += categorieAge(age);
     return rapport;
   }
 
+  console.log(genererRapportUtilisateur("Lucía", 25, "Madrid", "Diseñadora", true));
